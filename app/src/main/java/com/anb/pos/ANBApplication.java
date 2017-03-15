29c3dd4aabb4b9babf1ee55b465bb5341d6e6e7b
Bhuamik <bhaumik.shah@indianic.com>
2017-03-15 19:09:49 +0530

    Change Login Form

---
+++ app/src/main/java/com/anb/pos/ANBApplication.java
@@ -0,0 +1,10 @@
+package com.anb.pos;
+
+import android.app.Application;
+
+/**
+ * Created by indianic on 15/03/17.
+ */
+
+public class ANBApplication extends Application {
+}
--- app/src/main/java/com/anb/pos/SplashActivity.java
+++ app/src/main/java/com/anb/pos/SplashActivity.java
@@ -1,13 +1,30 @@
 package com.anb.pos;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Handler handler;
+    private int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handler = new Handler();
+        handler.postDelayed(runnable, SPLASH_TIME_OUT);
     }
+
+    final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            final Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
 }
